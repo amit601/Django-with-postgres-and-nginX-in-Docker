c9119f50ead4b75258f07dd19d37e3ad27f07059
Amit Kumar Yadav <itamit2017@gmail.com>
2020-05-14 17:03:37 +0530

    complete solution

--- start-DC.sh
+++ start-DC.sh
@@ -3,5 +3,7 @@
 ##
 
 printf "\nStopping Docker-Compose\n"
+## add --scale app=count  to scale up the application.
+## sudo docker-compose up -d  --scale djangoapp=4 will launch 4 container for django app.
 sudo docker-compose up -d
 printf "\nDocker service stack is Stopped\n"
